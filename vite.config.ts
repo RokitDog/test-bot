9583c0c8b30d1f0f88dac7e0ff8a83e6518f2df3
RokitDog <bojanforgaming@hotmail.com>
2024-03-03 17:11:39 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js"
 
 export default defineConfig({
-	plugins: [react(), cssInjectedByJsPlugin()],
+	plugins: [react()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
