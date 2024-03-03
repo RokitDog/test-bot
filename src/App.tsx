b40591010ea6da7eda9247bc2921bd942f3013d1
RokitDog <bojanforgaming@hotmail.com>
2024-03-03 16:54:15 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-// import { ChatPanel } from "./components/chat-panel"
+import { ChatPanel } from "./components/chat-panel"
 
 function App() {
 	return (
 		<>
-			<h1 className="text-3xl">TESTING</h1>
+			<ChatPanel />
 		</>
 	)
 }
