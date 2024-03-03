6908d60333d57ef7b14436588025c61608603f15
RokitDog <bojanforgaming@hotmail.com>
2024-03-03 16:58:27 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
 import { ChatPanel } from "./components/chat-panel"
+import { Button } from "./components/ui/button"
 
 function App() {
+	const [visible, setVisible] = useState(false)
+
 	return (
-		<>
-			<ChatPanel />
-		</>
+		<div className="absolute bottom-12 right-12 flex flex-col items-end gap-3.5">
+			{visible && <ChatPanel />}
+			<Button
+				onClick={() => {
+					setVisible(!visible)
+				}}
+				className="max-w-[100px]"
+			>
+				Click Me
+			</Button>
+		</div>
 	)
 }
 
